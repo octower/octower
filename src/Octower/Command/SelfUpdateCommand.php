a8c891927e819c44f4c78413ca68c485525b2943
William POTTIER <wpottier@clever-age.com>
2013-08-26 02:59:24 +0200

    change download update to http and not https protocol

--- src/Octower/Command/SelfUpdateCommand.php
+++ src/Octower/Command/SelfUpdateCommand.php
@@ -30,7 +30,6 @@ class SelfUpdateCommand extends Command
     {
         $this
             ->setName('self-update')
-            ->setAliases(array('selfupdate'))
             ->setDescription('Updates octower.phar to the latest version.')
             ->setHelp(<<<EOT
 The <info>self-update</info> command checks getoctower.org for newer
@@ -39,27 +38,27 @@ versions of composer and if found, installs the latest.
 <info>php octower.phar self-update</info>
 
 EOT
-            )
-        ;
+            );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $localFilename = realpath($_SERVER['argv'][0]) ?: $_SERVER['argv'][0];
-        $tempFilename = dirname($localFilename) . '/' . basename($localFilename, '.phar').'-temp.phar';
+        $localFilename = realpath($_SERVER['argv'][0]) ? : $_SERVER['argv'][0];
+        $tempFilename  = dirname($localFilename) . '/' . basename($localFilename, '.phar') . '-temp.phar';
 
         // check for permissions in local filesystem before start connection process
         if (!is_writable($tempDirectory = dirname($tempFilename))) {
-            throw new FilesystemException('Octower update failed: the "'.$tempDirectory.'" directory used to download the temp file could not be written');
+            throw new FilesystemException('Octower update failed: the "' . $tempDirectory . '" directory used to download the temp file could not be written');
         }
 
         if (!is_writable($localFilename)) {
-            throw new FilesystemException('Octower update failed: the "'.$localFilename. '" file could not be written');
+            throw new FilesystemException('Octower update failed: the "' . $localFilename . '" file could not be written');
         }
 
-        $protocol = extension_loaded('openssl') ? 'https' : 'http';
-        $rfs = new RemoteFilesystem($this->getIO());
-        $latest = trim($rfs->getContents('getcomposer.org', $protocol . '://getcomposer.org/version', false));
+        //$protocol = extension_loaded('openssl') ? 'https' : 'http';
+        $protocal = 'http';
+        $rfs      = new RemoteFilesystem($this->getIO());
+        $latest   = trim($rfs->getContents('getcomposer.org', $protocol . '://getcomposer.org/version', false));
 
         if (Octower::VERSION !== $latest) {
             $output->writeln(sprintf("Updating to version <info>%s</info>.", $latest));
@@ -86,7 +85,7 @@ EOT
                 if (!$e instanceof \UnexpectedValueException && !$e instanceof \PharException) {
                     throw $e;
                 }
-                $output->writeln('<error>The download is corrupted ('.$e->getMessage().').</error>');
+                $output->writeln('<error>The download is corrupted (' . $e->getMessage() . ').</error>');
                 $output->writeln('<error>Please re-run the self-update command to try again.</error>');
             }
         } else {
