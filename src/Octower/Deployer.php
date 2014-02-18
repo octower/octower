67a581c4d0ebe21f51988e8b2e652a99876585ed
William POTTIER <wpottier@users.noreply.github.com>
2014-02-18 14:47:43 +0100

    fix bad syntax

--- src/Octower/Deployer.php
+++ src/Octower/Deployer.php
@@ -175,7 +175,7 @@ class Deployer
             $this->getGenerator($sharedObject['generator'])->update($filesystem, $releasePath, $sharedPath, $shared);
         }
 
-        if (file_exists($sharedInReleasePath) && (!is_link($sharedInReleasePath) || readlink($sharedInReleasePath) !== $sharedPath))) {
+        if (file_exists($sharedInReleasePath) && (!is_link($sharedInReleasePath) || readlink($sharedInReleasePath) !== $sharedPath)) {
             // Error file or directory exist
             throw new \RuntimeException(sprintf('File, directory or other symbolic link allready exist at %s. Remove or move them to proceed to release activation.', $sharedInReleasePath));
         }
