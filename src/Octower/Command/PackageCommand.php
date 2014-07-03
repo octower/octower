7d2d994c9dc360ac3cad677ac8cec3c9a4fede89
William POTTIER <wpottier@users.noreply.github.com>
2014-07-03 10:58:10 +0200

    Update PackageCommand.php

--- src/Octower/Command/PackageCommand.php
+++ src/Octower/Command/PackageCommand.php
@@ -24,7 +24,7 @@ class PackageCommand extends Command
         $this
             ->setName('package:generate')
             ->setDescription('Create package to deploy')
-            ->addOption('version', null, InputOption::VALUE_OPTIONAL)
+            ->addOption('force-version', 'fv', InputOption::VALUE_OPTIONAL)
             ->setHelp(<<<EOT
 <info>php octower.phar package:generate</info>
 EOT
