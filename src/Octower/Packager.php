dd35edcdd5aa07636c677e34c3523a62259c996e
William Pottier <wpottier@clever-age.com>
2015-02-23 16:01:50 +0100

    shutoff ZipArchive php warning. Error allready handled by checking metadata validity (fix #15)

--- src/Octower/Packager.php
+++ src/Octower/Packager.php
@@ -78,8 +78,8 @@ class Packager
         /** @var \Phar $phar */
         $archive = new \ZipArchive();
         $archive->open($package);
-
-        $metadata = JsonFile::parseJson($archive->getArchiveComment());
+        
+        $metadata = JsonFile::parseJson(@$archive->getArchiveComment());
 
         if (!$metadata || !isset($metadata['version'])) {
             throw new \Exception('Invalid package metadata');
